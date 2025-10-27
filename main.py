f6568ba1bd82d04ae39d4eb493cfe0475fff9d12
Abid Hussain <abid.geek@outlook.com>
2025-10-27 09:27:43 -0400

    Implemented wall following

--- main.py
+++ main.py
@@ -128,7 +128,6 @@ motor_left.stop()
 motor_right.run_time(-BACKUP_SPEED, 2_000, wait=False)
 motor_left.run_time(-BACKUP_SPEED, 2_000)
 
-<<<<<<< HEAD
 
 # Turns robot right 90 degrees
 motor_right.run_time(-RIGHT_TURN_AV, 2_000, wait=False)
@@ -138,48 +137,3 @@ wait(500)
 
 # Follows wall for 2.2 meters
 follow_wall(2.2)
-=======
-# Go back 15cm
-# Run motors to go back in 3 seconds
-backwards_speed = (180/math.pi)*( (0.30 / 3) / (WHEEL_DIAMETER / (2 * 1000)))
-motor_right.run_time(-backwards_speed, 3_000, wait=False)
-motor_left.run_time(-backwards_speed, 3_000, wait=False)
-
-wait(1_000)
-ev3.speaker.beep()
-
-# turn 90 degrees to the right      
-turn_speed = FRICTION*90/1_500
-motor_right.run_time(turn_speed, 1_500, wait=False)
-motor_left.run_time(-turn_speed, 1_500, wait=False)
-wait(1_500)
-ev3.speaker.beep()
-
-# follow the wall until have travelled 2.2 meters in positive x direction
-distance_travelled = 0
-while distance_travelled < 2_200:
-    distance_to_wall = ultrasonic_sensor.distance()
-    error = distance_to_wall - 200  # target distance is 250 mm
-    Kp = 1.2  # Proportional gain
-    correction = Kp * error
-
-    left_speed = FOLLOW_WALL_SPEED + correction
-    right_speed = FOLLOW_WALL_SPEED - correction
-
-    motor_left.run(left_speed)
-    motor_right.run(right_speed)
-
-    # ensure to update distance_travelled only in the positive x direction, none in the y direction
-    if left_speed > right_speed:
-        distance_travelled += (left_speed * 10) / 1000  # Convert mm/s to mm per 10 ms
-    else:
-        distance_travelled += (right_speed * 10) / 1000  # Convert mm/s to mm per 10 ms
-
-    wait(10)
-motor_right.stop()
-motor_left.stop()
-
-ev3.speaker.beep()
-wait(100)
-ev3.speaker.beep()
->>>>>>> 9dc2b201d5d9276dda8b9ae178841af1a69ea8c4
